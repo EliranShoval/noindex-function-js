e7bfe24f398b68d2d52f38e6addc1e21dee2c8c3
Eliran Shoval  | SEO Wizerd <91342195+EliranShoval@users.noreply.github.com>
2022-05-06 10:37:31 +0300

    Add files via upload

---
+++ setnoindex.js
@@ -0,0 +1,14 @@
+
+
+//Copy this function with script tags - <script></script>
+//Feel free to try it! good luck :)
+function setNoIndex(){
+
+  var headNoIndex = document.getElementsByTagName('head')[0];
+  var noIndexTag = document.createElement('meta');
+  noIndexTag.setAttribute('name','robots');
+  noIndexTag.setAttribute('content','noindex follow');
+  headNoIndex.appendChild(noIndexTag);
+}
+
+setNoIndex();
